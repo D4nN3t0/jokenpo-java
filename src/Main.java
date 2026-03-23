7905bb1e3f7debb747c1b9a9e92e6901a746a5bc
Daniel Avelino <daniel.avelinoneto@gmail.com>
2026-03-23 15:54:15 -0300

    feat: adiciona sistema de pontuação e modo melhor de 3

--- src/Main.java
+++ src/Main.java
@@ -8,50 +8,110 @@ public class Main {
         Random gerador = new Random();
         Scanner leia = new Scanner(System.in);
 
-        String[] opcoes = {"Pedra", "Papel", "Tesoura"};
+        String[] jokenpo = {"Pedra", "Papel", "Tesoura"};
 
-        boasvindas();
-        int com = gerador.nextInt(3);
-        int player = leia.nextInt();
+        int com;
+        int player;
+        int scorePlayer = 0, scoreMachine = 0;
+        boolean leave = false;
 
-        if (player == 0) {
-            System.out.println("Saindo...");
-            return;
-        }
+        int partida = 0;
 
-        player -= 1;
-        rodada(opcoes, player, com);
+        do {
+            boasvindas();
 
-        if (player == com) {
-            mensagemDeEmpate(gerador);
-        } else if ((player == 0 && com == 2) || (player == 1 && com == 0) || (player == 2 && com == 1)) {
-            mensagemDoGanhador(gerador);
-        } else {
-            mensagemDePerda(gerador);
-        }
+            int opcao = leia.nextInt();
+
+            switch (opcao) {
+                case 0:
+                    System.out.println("Saindo...");
+                    leave = true;
+                    break;
+
+                case 1:
+                case 2:
+                case 3:
+                    player = opcao - 1;
+                    com = gerador.nextInt(3);
+
+                    rodada(jokenpo, player, com);
+
+                    int resultado = iniciarRound(player, com, gerador);
+
+                    if (resultado == 1) {
+                        scorePlayer++;
+                    } else if (resultado == -1) {
+                        scoreMachine++;
+                    }
+
+                    partida++;
+
+                    // lógica melhor de 3
+                    if (partida == 3 || scorePlayer == 2 || scoreMachine == 2) {
+                        roundScore(scoreMachine, scorePlayer);
+                        System.out.println("\nTemos um ganhador..?");
+
+                        if (scoreMachine > scorePlayer) {
+                            System.out.println("A Máquina levou a vitória para casa dessa vez!");
+                        } else if (scorePlayer > scoreMachine){
+                            System.out.println("Parabéns pela sua vitória, você ganhou da máquina!");
+                        } else {
+                            System.out.println("Houve um Empate!!");
+                            System.out.println("Não foi dessa vez, tente novamente.");
+                        }
+
+                        leave = true;
+                    }
+                    break;
+
+                case 4:
+                    roundScore(scoreMachine, scorePlayer);
+                    break;
+
+                default:
+                    System.out.println("Opção inválida. Tente novamente");
+            }
+
+        } while (!leave);
 
         leia.close();
-
     }
 
     public static void boasvindas() {
         System.out.println("""
                 ================================
                 Bem-vindo ao Jogo de JOKENPO!
-                Você Irá jogar contra a máquina.
+                Você irá jogar contra a máquina.
+                Modo de jogo: MELHOR DE 3
                 ================================
                 Escolha sua opção:
                 [0] Sair
                 [1] Pedra
                 [2] Papel
                 [3] Tesoura
+                [4] Mostrar a pontuação
                 ================================
                 Digite a opção:""");
     }
 
-    public static void rodada (String[] opcoes, int player, int com) {
-        System.out.println("Você escolheu: " + opcoes[player]);
-        System.out.println("O computador escolheu: " + opcoes[com]);
+    public static void rodada(String[] jokenpo, int player, int com) {
+        System.out.println("Você escolheu: " + jokenpo[player]);
+        System.out.println("O computador escolheu: " + jokenpo[com]);
+    }
+
+    public static int iniciarRound(int player, int com, Random gerador) {
+        if (player == com) {
+            mensagemDeEmpate(gerador);
+            return 0; // empate
+        } else if ((player == 0 && com == 2) ||
+                (player == 1 && com == 0) ||
+                (player == 2 && com == 1)) {
+            mensagemDoGanhador(gerador);
+            return 1; // player ganhou
+        } else {
+            mensagemDePerda(gerador);
+            return -1; // máquina ganhou
+        }
     }
 
     public static void mensagemDePerda(Random gerador) {
@@ -62,7 +122,6 @@ public class Main {
                 "Você foi superado! Tentar novamente?"
         };
 
-        // Seleciona um índice aleatório de 0 até o tamanho do array - 1
         String rodada = mensagem[gerador.nextInt(mensagem.length)];
         System.out.println(rodada);
     }
@@ -74,7 +133,6 @@ public class Main {
                 "De novo! Escolham sua melhor tática novamente."
         };
 
-        // Seleciona um índice aleatório de 0 até o tamanho do array - 1
         String rodada = mensagem[gerador.nextInt(mensagem.length)];
         System.out.println(rodada);
     }
@@ -87,8 +145,12 @@ public class Main {
                 "Vitória Real! A CPU foi completamente dominada."
         };
 
-        // Seleciona um índice aleatório de 0 até o tamanho do array - 1
         String rodada = mensagem[gerador.nextInt(mensagem.length)];
         System.out.println(rodada);
+    }
+
+    public static void roundScore(int scoreMachine, int scorePlayer) {
+        System.out.printf("Computador: %d Pontos\n", scoreMachine);
+        System.out.printf("Player: %d Pontos\n", scorePlayer);
     }
 }
